8fb2d08e2186bb9d38be35369334df3028b139aa
crystal alice <crystalalice21@gmail.com>
2020-04-10 14:35:56 +0300

    adds class

---
+++ src/app/github-class/github-class.spec.ts
@@ -0,0 +1,7 @@
+import { GithubClass } from './github-class';
+
+describe('GithubClass', () => {
+  it('should create an instance', () => {
+    expect(new GithubClass()).toBeTruthy();
+  });
+});
---
+++ src/app/github-class/github-class.ts
@@ -0,0 +1,11 @@
+export class GithubClass {
+  constructor(
+    public name: string,
+    public avatar_url: (string),
+    public followers: string,
+    public following: string,
+    public html_url: string,
+    public login: string,
+
+){}
+}
